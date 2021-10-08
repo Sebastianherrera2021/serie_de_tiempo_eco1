fe7388096d512ca48a8c040ea0749f5758398d8c
Sebastian <mediclifeee@gmail.com>
2021-10-08 01:37:55 -0500

    arreglooooo

--- prueba1.R
+++ prueba1.R
@@ -1,8 +1,16 @@
 library(readxl)
-datos <- read_excel("C:/Users/Usuario/Downloads/economic.xlsx")
-View(datos)
-attach(datos)
-names(datos)
-inflacion<-datos[,-1]
-colnames(datos)=c("Año","Inflacion","Salario nominal","PIB municipal 2018")#cambiar nombre de las variables porque son muy largos
-INFLACION2=data.frame(INFLACION,`Gastos)
+library(tidyverse)
+library(lubridate)
+library(readxl)
+econometriatabla2 <- read_excel("C:/Users/Usuario/Downloads/econometriatabla2.xlsx")
+View(econometriatabla2)
+attach(econometriatabla2)
+colnames(econometriatabla2)=c("inflacion","Salario nominal","Gasto publico","Tipo de cambio real","Deficit o superavit fiscal","Masa monetaria (M1)")
+names(econometriatabla2)
+
+##como serie de tiempo
+#no es linea recta, exponencial,
+inflacion.ts=ts(econometriatabla2,start=c(2000) , freq=12 )
+inflacion.ts
+plot(inflacion.ts)
+summary(econometriatabla2)
